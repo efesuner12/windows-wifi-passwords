f47f89fe1d9505e7e4bd2e9c768cb6995a1c3b62
Efe Suner <efesuner2001@hotmail.com>
2021-11-28 13:29:14 +0000

    Add files via upload

---
+++ displayPassword.py
@@ -0,0 +1,25 @@
+import subprocess
+
+def displayWifiPass():
+    passwords = ""
+
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
+    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+
+    for i in profiles:
+        try:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
+            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+            
+            try:
+                passwords += "{:<30}|  {:<}\n".format(i, results[0])
+            except IndexError:
+                passwords += "{:<30}|  {:<}\n".format(i, "")
+
+        except subprocess.CalledProcessError:
+            passwords += "{:<30}|  {:<}\n".format(i, "*****ENCODING ERROR*****")
+
+    return passwords
+
+if __name__ == "__main__":
+    print(displayWifiPass())
